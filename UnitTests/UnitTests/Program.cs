d5b6f6a73ca885c40163cf10051e8fb0fb6c477e
Benjamin <h8bmf@hotmail.co.uk>
2021-07-27 14:21:53 +0100

    UnitTestLesson Refactored

--- UnitTests/UnitTests/Program.cs
+++ UnitTests/UnitTests/Program.cs
@@ -6,7 +6,27 @@ namespace UnitTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int timeOfDay = 21;
+            var greeting = Greeting(timeOfDay);
+            Console.WriteLine(greeting);
+            Console.WriteLine(Greeting(timeOfDay));
+        }
+        private static string Greeting(int timeOfDay)
+        {
+            string greeting;
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                greeting = "Good moring!";
+            }
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                greeting = "Good afternoon!";
+            }
+            else
+            {
+                greeting = "Good evening!";
+            }
+            return greeting;
         }
     }
 }
